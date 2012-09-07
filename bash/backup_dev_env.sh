009efc8ade6976274da5c1b300de70142aebf931
spatchcock <andrew.berkeley.is@googlemail.com>
2012-09-07 11:25:13 +0100

    new dev env back up

---
+++ bash/backup_dev_env.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Pass in the name of the directory to copy contents to.
+
+IFS_DEFAULT="$IFS";
+IFS=",";
+
+files=(code,"VirtualBox VMs",.gem,.heroku,.ssh,.vagrant.d,.VirtualBox,.bashrc,.gemrc,.gitconfig,.irbrc)
+
+target_dir="$1";
+mkdir $target_dir;
+
+for i in ${files[@]}
+do
+  cp -rf "$i" $target_dir;
+done;
+
+IFS=$IFS_DEFAULT;
