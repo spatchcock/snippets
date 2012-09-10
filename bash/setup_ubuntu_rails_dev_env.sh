9d23b22e9afb4e2ead9f3b6f91e80967729ea0e6
spatchcock <andrew.berkeley.is@googlemail.com>
2012-09-10 22:03:52 +0100

    fix sublime text install command

--- bash/setup_ubuntu_rails_dev_env.sh
+++ bash/setup_ubuntu_rails_dev_env.sh
@@ -42,7 +42,7 @@ sudo gem install rails vagrant bundler
 ### Add Sublime Text 2 ###
 sudo add-apt-repository ppa:webupd8team/sublime-text-2
 sudo apt-get update
-sudo apt-get install sublime-text-2
+sudo apt-get install sublime-text
 
 # Add Git Support#
 cd ~/.config/sublime-text-2/Packages/
